d1dce9acab5b425bce558f675ed4585a0368af94
Gerard Paul P. Labitad <gerardpaul.labitad@outlook.com>
2013-06-18 13:34:35 +0800

    Create csv_convert_sql.py

---
+++ csv_convert_sql.py
@@ -0,0 +1,46 @@
+import csv
+
+def read_csv(infilename,outfilename):
+  with open(infilename, 'rb') as csvfile:
+		doc = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+		rownum = 0
+		
+		values = []
+		for row in doc:
+			if rownum == 0:
+				contents = row
+				contents = ",".join(row)
+				values.insert(rownum, contents)
+			else:
+				value = [] 
+				colnum = 0
+				for col in row:
+					if colnum == 0 or colnum == 7 or colnum == 8:
+						value.append("'" + col + "'")
+					else:
+						value.append(col)
+					colnum += 1
+				value = ",".join(value)
+				values.insert(rownum, value)
+			rownum += 1
+			
+		write_sql(values, outfilename, rownum)
+		
+def write_sql(values, outfilename, rownum):
+	sql_file = open(outfilename, 'wb')
+	i = 1
+	while i < rownum:
+		insert = "INSERT INTO xyz(" + values[0] + ") VALUES(" + values[i] + ");"
+		sql_file.write("%s\n" % (insert));
+		i += 1;
+		
+	sql_file.close()
+	
+def main():
+	str = raw_input("CSV Filename: ");
+	infilename = str + '.csv'
+	#str = raw_input("Output Filename: ");
+	outfilename = str + '.sql'
+	read_csv(infilename,outfilename)
+
+main()
